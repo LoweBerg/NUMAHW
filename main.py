a0cb04c87c2f885b9c3edeaf9c67b9daf3fa2d9e
Lowe Berg <loweberg04@gmail.com>
2025-04-04 16:09:56 +0200

    tog bort en print

--- main.py
+++ main.py
@@ -50,7 +50,6 @@ def main():
     for i in range(2, 7):
         for j in range(np.size(x)):
             error[j] = abs(np.log(x[j]) - fast_approx_ln(x[j], i))
-        print(error[1], error[4], error[10])
         plt.scatter(x, error, color=colors[i-2], label=f'iteration {i}', marker='o', s=5)
     plt.xlabel('x')
     plt.xlim(0, 20)
